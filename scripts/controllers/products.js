a2445fcce6fcaa21a8fdfd44a4203b1241709a5c
Ricardo <richard11_71@outlook.com>
2016-10-17 23:35:53 -0500

    create a filter category to filter the products array based on categories dropdown

--- scripts/controllers/products.js
+++ scripts/controllers/products.js
@@ -1,6 +1,7 @@
 angular.module("Store")
 	.controller("ProductsCtrl", ["$scope", "productsService", function ProductsCtrl($scope, productsService) {
 		$scope.myCart = productsService.getProductsInCart();
+		$scope.categoryFilter = "all";
 
 		// Retrieve products from json file
 		productsService.fetchProducts()
@@ -29,3 +30,66 @@ angular.module("Store")
 			productsService.addToCart(product);
 		}
 	}])
+
+	.filter("filterByCategory", function() {
+		return function(items, category) {
+			var filtered = [];
+			return filter(items, filtered, category);
+		};
+	})
+
+function filter(items, arr, category) {
+	switch( category ) {
+		case "all":
+			arr = items;
+			break;
+		case "available":
+		case "best_seller":
+			for (var i = 0; i < items.length; i++) {
+				if (items[i][category]) {
+					arr.push(items[i]);
+				};
+			};
+			break;
+		case "not_available":
+			for (var i = 0; i < items.length; i++) {
+				// Push object to filtered array if not available
+				if (!items[i].available) {
+					arr.push(items[i]);
+				};
+			};
+			break;
+		case "30000":
+			// Convert string price to integer
+			var price = parseInt(category);
+			for (var i = 0; i < items.length; i++) {
+				if (items[i].price > price) {
+					arr.push(items[i]);
+				};
+			};
+			break;
+		case "10000":
+			// Convert string price to integer
+			var price = parseInt(category);
+			for (var i = 0; i < items.length; i++) {
+				if (items[i].price < price) {
+					arr.push(items[i]);
+				};
+			};
+			break;
+		default:
+			for (var i = 0; i < items.length; i++) {
+				// Get the categories array of the current object
+				var categories = items[i].categories;
+
+				for (var j = 0; j < categories.length; j++) {
+					// Check if the category selected by user match one of
+					// the current object
+					if(categories[j] == category) {
+						arr.push(items[i]);
+					}
+				}
+			};
+	};
+	return arr;
+}
