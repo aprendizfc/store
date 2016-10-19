98b76d904daf0e2e832b68035348f5f1540ea527
Ricardo <richard11_71@outlook.com>
2016-10-19 01:44:38 -0500

    add comments

--- scripts/controllers/product-detail.js
+++ scripts/controllers/product-detail.js
@@ -1,8 +1,12 @@
 angular.module("Store")
 	.controller("ProductDetailCtrl", ["$scope", "$stateParams", "productsService", function($scope, $stateParams, productsService) {
+		// Get all the products availables in the cart
 		$scope.myCart = productsService.getProductsInCart();
+
+		// Get the route param which is the product id
 		var param = $stateParams.productId;
 
+		// Get the product based on param variable
 		$scope.product = productsService.getProductById(param);
 
 		$scope.addToCart = function(product) {
